dc20560d08331e7097e9031a469c5bcdd0902652
Devesh <devesh@Deveshs-Laptop.local>
2024-12-10 00:05:41 +0530

    changed base url

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://basedwrapped.xyz';
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://basedwrapped.xyz';
+// const BASE_URL = 'http://localhost:8000';
 
 export const SIP_STATUS = {
   ACTIVE: "active",
